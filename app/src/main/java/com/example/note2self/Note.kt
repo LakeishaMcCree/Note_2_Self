5f97d90332600c9e6dfe58b38a65d7b2af6c3004
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2021-04-22 15:21:56 -0400

    created String resources Created a new note file with 5 properties and added a new layout resource file

---
+++ app/src/main/java/com/example/note2self/Note.kt
@@ -0,0 +1,9 @@
+package com.example.note2self
+
+class Note {
+    var title: String? = null
+    var description: String? = null
+    var idea: Boolean = false
+    var todo: Boolean = false
+    var important: Boolean = false
+}
