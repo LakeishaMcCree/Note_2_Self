553bc82f256445597cb2d6bdf68a67c08cd57224
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2021-04-26 21:09:56 -0400

    declared and initialized an instance of AlertDialog.Builder and LayoutInflater to create a View object that has the layout for the dialog.

--- app/src/main/java/com/example/note2self/DialogShowNote.kt
+++ app/src/main/java/com/example/note2self/DialogShowNote.kt
@@ -9,7 +9,11 @@ class DialogShowNote : DialogFragment (){
     private var note: Note? = null
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         //All the other code goes here
+        val builder = AlertDialog.Builder(this.activity!!)
 
+        val inflater = activity!!.layoutInflater
+
+        val dialogView = inflater.inflate(R.layout.dialog_show_note, null)
     }
 
     //Receive a note from the MainActivity class
