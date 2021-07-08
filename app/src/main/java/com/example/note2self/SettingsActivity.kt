353afd53286cccb9acb414b404a33d7c7da1591a
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2021-07-08 19:35:13 -0400

    Added functionality to the switch widget

--- app/src/main/java/com/example/note2self/SettingsActivity.kt
+++ app/src/main/java/com/example/note2self/SettingsActivity.kt
@@ -1,11 +1,35 @@
 package com.example.note2self
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.SharedPreferences
+
 
 class SettingsActivity : AppCompatActivity() {
+
+    private var showDividers: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val prefs = getSharedPreferences(
+            "Note 2 Self", Context.MODE_PRIVATE
+        )
+
+        showDividers = prefs.getBoolean("dividers", true)
+
+        //set the switch to on or off as appropriate
+        switch1.ischecked = showDividers
+
+        switch1.setOnCheckedChangeListener {
+            buttonView, isChecked ->
+
+            showDividers = isChecked
+        }
+
     }
+
+
 }
